da951fb414a8f309ea5a39c1abc9e69f5e29f9e8
IMS <jose8055@gmail.com>
2019-02-18 16:14:33 -0500

    Initial Products fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MyAngularClient';
+export class AppComponent implements OnInit {
+  public title = 'My Angular App';
+  public isAuthenticated: boolean;
+
+  constructor(public oktaAuth: OktaAuthService) {
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+  login() {
+    this.oktaAuth.loginRedirect();
+
+  }
+
+  logout() {
+    this.oktaAuth.logout('/');
+
+  }
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
 }
